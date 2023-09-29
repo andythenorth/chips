b971829223d4b8de94924f24f74e6240a6575ece
Andy <andy@teamrubber.com>
2023-09-29 22:57:39 +0100

    Codechange: move crane rails to overlay sprites list

--- src/stations/harbour_crane.py
+++ src/stations/harbour_crane.py
@@ -46,38 +46,38 @@ def main():
 
     facility_type.add_spritelayout(
         id="harbour_crane_spritelayout_1_track",
-        ground_overlay_sprites=[],
-        rear_structure_sprites=["sprite_rails_for_track_tile", "sprite_2"],
+        ground_overlay_sprites=["sprite_rails_for_track_tile"],
+        rear_structure_sprites=["sprite_2"],
         main_structure_sprites=["sprite_1"],
     )
     facility_type.add_spritelayout(
         id="harbour_crane_spritelayout_2_track",
-        ground_overlay_sprites=[],
-        rear_structure_sprites=["sprite_rails_for_track_tile", "sprite_4"],
+        ground_overlay_sprites=["sprite_rails_for_track_tile"],
+        rear_structure_sprites=["sprite_4"],
         main_structure_sprites=["sprite_3"],
     )
     facility_type.add_spritelayout(
         id="harbour_crane_spritelayout_1_non_track",
-        ground_overlay_sprites=[],
-        rear_structure_sprites=["sprite_rails_for_non_track_tile", "sprite_2"],
+        ground_overlay_sprites=["sprite_rails_for_non_track_tile"],
+        rear_structure_sprites=["sprite_2"],
         main_structure_sprites=["sprite_1"],
     )
     facility_type.add_spritelayout(
         id="harbour_crane_spritelayout_2_non_track",
-        ground_overlay_sprites=[],
-        rear_structure_sprites=["sprite_rails_for_non_track_tile", "sprite_4"],
+        ground_overlay_sprites=["sprite_rails_for_non_track_tile"],
+        rear_structure_sprites=["sprite_4"],
         main_structure_sprites=["sprite_3"],
     )
     facility_type.add_spritelayout(
         id="harbour_crane_spritelayout_crane_rails_track",
-        ground_overlay_sprites=[],
-        rear_structure_sprites=["sprite_rails_for_track_tile"],
+        ground_overlay_sprites=["sprite_rails_for_track_tile"],
+        rear_structure_sprites=[],
         main_structure_sprites=[],
     )
     facility_type.add_spritelayout(
         id="harbour_crane_spritelayout_crane_rails_non_track",
-        rear_structure_sprites=["sprite_rails_for_non_track_tile"],
-        ground_overlay_sprites=[],
+        ground_overlay_sprites=["sprite_rails_for_non_track_tile"],
+        rear_structure_sprites=[],
         main_structure_sprites=[],
     )
 
