895f51439e0d7f7a9922551cb5187a9f860a7f9f
Andy <andy@teamrubber.com>
2023-10-02 08:33:18 +0100

    Change: add a second foundry tile

--- src/stations/foundry_building_1.py
+++ src/stations/foundry_building_1.py
@@ -8,29 +8,65 @@ def main(numeric_id):
     )
 
     facility_type.add_sprite(
-        id="sprite_building",
+        id="ground_overlay_platforms",
+        x_y_loc=(10, 290),
+        dimensions=(64, 31),
+        offsets=(-31, 0),
+    )
+    facility_type.add_sprite(
+        id="ground_overlay_whole_tile",
+        x_y_loc=(10, 250),
+        dimensions=(64, 31),
+        offsets=(-31, 0),
+    )
+    facility_type.add_sprite(
+        id="sprite_building_large",
         x_y_loc=(10, 10),
         dimensions=(64, 145),
         offsets=(-31, -114),
     )
     facility_type.add_sprite(
-        id="sprite_rear_legs",
+        id="sprite_rear_legs_large",
         x_y_loc=(10, 170),
         dimensions=(64, 65),
         offsets=(-31, -34),
     )
     facility_type.add_sprite(
-        id="ground_overlay",
-        x_y_loc=(10, 250),
-        dimensions=(64, 31),
-        offsets=(-31, 0),
+        id="sprite_building_small",
+        x_y_loc=(150, 10),
+        dimensions=(64, 145),
+        offsets=(-31, -114),
+    )
+    facility_type.add_sprite(
+        id="sprite_rear_legs_small",
+        x_y_loc=(150, 170),
+        dimensions=(64, 65),
+        offsets=(-31, -34),
     )
 
     facility_type.add_spritelayout(
-        id="foundry_building_1_spritelayout_1",
-        ground_overlay_sprites=["ground_overlay"],
-        rear_structure_sprites=["sprite_rear_legs"],
-        main_structure_sprites=["sprite_building"],
+        id="foundry_building_1_spritelayout_1_track",
+        ground_overlay_sprites=["ground_overlay_platforms"],
+        rear_structure_sprites=["sprite_rear_legs_large"],
+        main_structure_sprites=["sprite_building_large"],
+    )
+    facility_type.add_spritelayout(
+        id="foundry_building_1_spritelayout_1_non_track",
+        ground_overlay_sprites=["ground_overlay_whole_tile"],
+        rear_structure_sprites=["sprite_rear_legs_large"],
+        main_structure_sprites=["sprite_building_large"],
+    )
+    facility_type.add_spritelayout(
+        id="foundry_building_1_spritelayout_2_track",
+        ground_overlay_sprites=["ground_overlay_platforms"],
+        rear_structure_sprites=["sprite_rear_legs_small"],
+        main_structure_sprites=["sprite_building_small"],
+    )
+    facility_type.add_spritelayout(
+        id="foundry_building_1_spritelayout_2_non_track",
+        ground_overlay_sprites=["ground_overlay_whole_tile"],
+        rear_structure_sprites=["sprite_rear_legs_small"],
+        main_structure_sprites=["sprite_building_small"],
     )
 
     facility_type.add_rail_station(
@@ -39,7 +75,7 @@ def main(numeric_id):
             (
                 0,
                 0,
-                "foundry_building_1_spritelayout_1",
+                "foundry_building_1_spritelayout_1_track",
             ),
         ],
     )
@@ -49,7 +85,27 @@ def main(numeric_id):
             (
                 0,
                 0,
-                "foundry_building_1_spritelayout_1",
+                "foundry_building_1_spritelayout_1_non_track",
+            ),
+        ],
+    )
+    facility_type.add_rail_station(
+        type="track_tile",
+        layout=[
+            (
+                0,
+                0,
+                "foundry_building_1_spritelayout_2_track",
+            ),
+        ],
+    )
+    facility_type.add_rail_station(
+        type="non_track_tile",
+        layout=[
+            (
+                0,
+                0,
+                "foundry_building_1_spritelayout_2_non_track",
             ),
         ],
     )
